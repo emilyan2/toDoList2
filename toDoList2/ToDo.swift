78cba8c8eaca460dd4bc111317f62e743717a873
Emily An <emilysuperhalo@gmail.com>
2022-08-03 13:33:07 -0400

    Initial Commit

---
+++ toDoList2/ToDo.swift
@@ -0,0 +1,13 @@
+//
+//  ToDo.swift
+//  toDoList2
+//
+//  Created by Scholar on 8/3/22.
+//
+
+import UIKit
+
+class ToDo {
+    var name = " "
+    var important = false
+}
---
+++ toDoList2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  toDoList2
+//
+//  Created by Scholar on 8/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
